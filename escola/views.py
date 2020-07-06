2a74ca90aef796b8dc7be885ce7e97c7351bfbed
Guilherme Lima <guilhermelimaruby@gmail.com>
2020-07-06 11:03:45 -0300

    aula_3

--- escola/views.py
+++ escola/views.py
@@ -1,6 +1,13 @@
-from django.http import JsonResponse
+from rest_framework import viewsets
+from escola.models import Aluno, Curso
+from escola.serializer import AlunoSerializer, CursoSerializer
 
-def alunos(request):
-    if request.method == 'GET':
-        aluno = {'id':1}
-        return JsonResponse(aluno)
+class AlunosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os alunos e alunas"""
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+
+class CursosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os cursos"""
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
--- setup/urls.py
+++ setup/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from django.urls import path
-from escola.views import alunos
+from django.urls import path,include
+from escola.views import AlunosViewSet, CursosViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('alunos', AlunosViewSet, basename='Alunos')
+router.register('cursos', CursosViewSet, basename='Cursos')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('alunos/', alunos)
+    path('', include(router.urls) )
 ]
